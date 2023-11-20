62710360fd85eb68315186c690b6042c25f3dc26
noisfischer <noisfischer@gmail.com>
2023-11-20 15:07:23 -0500

    Spawn a checkpoint BP in front of the viewport camera

--- spawnCheckpoint.py
+++ spawnCheckpoint.py
@@ -1,30 +1,36 @@
 import unreal
 
-# Get Editor World
+# Get the current editor world
 editor_world = unreal.EditorLevelLibrary.get_editor_world()
 
-#Get the editor viewport client
+# Access the Level Editor Subsystem to interact with the editor's level-related functionalities.
 levelSubSys = unreal.get_editor_subsystem(unreal.LevelEditorSubsystem)
+
+# Retrieve current camera position and rotation from the editor's viewport.
 viewport = unreal.get_editor_subsystem(unreal.UnrealEditorSubsystem).get_level_viewport_camera_info()
 
+# Extract the camera's location and rotation from the viewport information.
 cameraLocation = viewport[0]
 cameraRotation = viewport[1]
 
-#Converts the rotation to a direction vector
+# Convert the camera's rotation to a forward direction vector.
+# This vector indicates the direction the camera is facing.
 cameraDirection = cameraRotation.get_forward_vector()
 
-#Specifies distance from camera at which to spawn the spot light
+# Define a distance from the camera where the new object will be spawned.
 distance = 500
 
-#Calculates spawn location
+# Calculate the spawn location for the new actor based on the camera's direction and the specified distance.
 spawnLocation = cameraLocation + cameraDirection * distance
 
-# Get Blueprint class
+# Load a Blueprint asset from the specified path in the Unreal project. In this case, 'BP_Checkpoint'.
 blueprint = unreal.EditorAssetLibrary.load_asset('/Game/Blueprints/BP_Checkpoint')
+
+# Get the class from the Blueprint asset. This class is used to spawn the actor.
 blueprint_class = unreal.load_class(None, blueprint.get_path_name())
 
-#Spawn the actor
-new_checkpoint= unreal.EditorLevelLibrary.spawn_actor_from_object(blueprint, spawnLocation)
+# Spawn an actor of the Blueprint class at the calculated spawn location in the editor world.
+new_checkpoint = unreal.EditorLevelLibrary.spawn_actor_from_object(blueprint, spawnLocation)
 
-# Output the name of the spawned actor (Optional)
+# Print the name of the spawned actor to the Output Log
 print(f'Spawned actor with name: {new_checkpoint.get_name()}')
